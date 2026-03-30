faef33a014dc9d4c88c282b53d3e27ae65ba638d
Antonella <divirgilioantonella@gmail.com>
2026-03-29 23:26:50 -0300

    feat(hooks): implement useInternetConnectionStatus hook with event listeners

--- src/hooks/useInternetStatus.ts
+++ src/hooks/useInternetStatus.ts
@@ -1,5 +1,19 @@
-export default function useInternetConnectionStatus() {
-  return {
+import { useEffect, useState } from 'react'
 
-  }
+export default function useInternetConnectionStatus() {
+  const [online, setOnline] = useState(navigator.onLine)
+
+  const handleOnlineStatus = async () => setOnline(navigator.onLine)
+
+  useEffect(() => {
+    globalThis.addEventListener('online', handleOnlineStatus)
+    globalThis.addEventListener('offline', handleOnlineStatus)
+
+    return () => {
+      globalThis.removeEventListener('online', handleOnlineStatus)
+      globalThis.removeEventListener('offline', handleOnlineStatus)
+    }
+  }, [])
+
+  return { online }
 }
