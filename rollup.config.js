6cb60dac2a880b23e6cfc60894da356c2274a09c
Antonella <divirgilioantonella@gmail.com>
2026-03-31 13:31:24 -0300

    chore(deps): add @rollup/plugin-terser for production bundle minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs"
+import terser from '@rollup/plugin-terser';
 import packageJson from "./package.json" with { type: "json" };
 
 export default [{
@@ -10,11 +11,13 @@ export default [{
       file: packageJson.main,
       format: "cjs",
       sourcemap: true,
+      plugins: [terser()]
     },
     {
       file: packageJson.module,
       format: "es",
       sourcemap: true,
+      plugins: [terser()]
   },
 ],
   plugins: [
